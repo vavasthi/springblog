4143b2a214ed040e919ecb5a8262e593bdd46fce
Vinay Avasthi <vinay@sanjnan.com>
2018-11-19 21:16:55 +0530

    Added initial code.

---
+++ tutorials/src/test/java/in/springframework/blog/tutorials/TutorialsApplicationTests.java
@@ -0,0 +1,16 @@
+package in.springframework.blog.tutorials;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TutorialsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
